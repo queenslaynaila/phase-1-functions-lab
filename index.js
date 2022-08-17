34ecb50d00f0a9c7cc25909a62be4a529c200567
Queenslay naila <queenslay.naila@student.moringaschool.com>
2022-08-17 03:10:30 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+  let distanceInBlocks = blocks - 42;
+  if (blocks < 42) {
+    distanceInBlocks = 42 - blocks;
+  }
+  return distanceInBlocks;
+}
+function distanceFromHqInFeet(blocks) {
+  let distanceInFeet = (blocks - 42) * 264;
+  if (blocks < 42) {
+    distanceInFeet = (42 - blocks) * 264;
+  }
+  return distanceInFeet;
+}
+
+function distanceTravelledInFeet(distance, destination) {
+  let distanceTravelledInFeet = (destination - distance) * 264;
+  if (destination < distance) {
+    distanceTravelledInFeet = (distance - destination) * 264;
+  }
+  return distanceTravelledInFeet;
+}
+
+function calculatesFarePrice(start, destination) {
+  let distanceTravelledInFeet = (start - destination) * 264;
+  if (start < destination) {
+    distanceTravelledInFeet = (destination - start) * 264;
+  }
+
+  if (distanceTravelledInFeet <= 400) {
+    return 0;
+  } else if (distanceTravelledInFeet > 400 && distanceTravelledInFeet <= 2000) {
+    return (distanceTravelledInFeet - 400) * 0.02;
+  } else if (
+    distanceTravelledInFeet > 2000 &&
+    distanceTravelledInFeet <= 2500
+  ) {
+    return 25;
+  } else {
+    return "cannot travel that far";
+  }
+}
+console.log(calculatesFarePrice(43, 44));
+console.log(calculatesFarePrice(34, 32));
+console.log(calculatesFarePrice(50, 58));
+console.log(calculatesFarePrice(34, 24));
